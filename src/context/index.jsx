3456a6b9131b3a22f6b7ccc79089415ffcbb9958
David gomez <david.gomez04@hotmail.com>
2024-03-30 16:29:00 -0500

    fix refactor code

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect} from "react";
 
 export const ShoppingCartContext = createContext();
 
@@ -56,6 +56,8 @@ export const ShoppingCartProvider = ({ children }) => {
         closeCheckoutSideMenu,
         order,
         setOrder,
+        items,
+        setItems
       }}
     >
       {children}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 import Card from "../../components/card";
 import Layout from "../../components/layout";
 import ProductDetail from "../../components/productDetail";
+import { ShoppingCartContext } from "../../context";
 
 function Home() {
-  
+  const context = useContext(ShoppingCartContext);
 
   return (
     <Layout>
       <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg place-items-center mb-20'>
-        {items?.map((item) => (
+        {context.items?.map((item) => (
           <Card
            key={item.id}
             data={item} />
